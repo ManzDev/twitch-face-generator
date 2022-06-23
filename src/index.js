a521a42e861a18039240519ac6120b4ef9b266c5
ManzDev <JoseRomanHernandez@gmail.com>
2022-06-23 23:36:58 +0100

    Feat: add button to generate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./components/FaceGenerator.js";
+
+const container = document.querySelector(".container");
+const faceButton = document.querySelector("button");
+
+faceButton.addEventListener("click", () => {
+  const oldFace = document.querySelector("face-generator");
+  oldFace.remove();
+  const newFace = document.createElement("face-generator");
+  container.insertAdjacentElement("afterbegin", newFace);
+});
